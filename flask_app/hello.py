1663610b06ea560b6d6e3c58f2b7c7a959dc85c3
Aldres98 <aldres98@gmail.com>
2020-10-05 02:07:45 +0300

    Update hello.py

--- flask_app/hello.py
+++ flask_app/hello.py
@@ -1,6 +1,20 @@
 from flask import Flask
+from datetime import datetime
+from flask import request
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/time')
+def time():
+    return datetime.utcnow()
+
+@app.route('/fingerprint')
+def fingerprint():
+    return request.headers.get('User-Agent')
+
+
